266d70d782998e8fe6cea19f7c8d3e067c8587b8
Craig Fitzgerald <craig.fitzgerald00@gmail.com>
2021-07-17 20:44:26 -0400

    auto gen url, colors

--- mandel.js
+++ mandel.js
@@ -9,7 +9,9 @@
 // http://gn00ltnd8rs6ph2/toys/mandel/mandel.html?i=5000&x0=-0.9974149939088077&y0=-0.28745231377667163&x1=-0.9974149936972826&y1=-0.2874523136558001&r=1&g=109&b=208&rd=0.449&gd=1.1609&bd=0.3352
 // http://gn00ltnd8rs6ph2/toys/mandel/mandel.html?i=5000&x0=-1.2242399925349&y0=-0.13112323809286083&x1=-1.2242399268802495&y1=-0.13112320057591773&r=197&g=23&b=208&rd=1.024&gd=2.14&bd=3.062
 // http://gn00ltnd8rs6ph2/toys/mandel/mandel.html?i=500&x0=-0.9974147551123084&y0=-0.28745249157767716&x1=-0.9974142757661713&y1=-0.2874522176655988&r=1&g=109&b=208&rd=3.357&gd=8.87&bd=7.562// 
-
+// http://gn00ltnd8rs6ph2/toys/mandel/mandel.html?i=500&x0=-0.7490424268348466&y0=-0.10138705912859394&x1=-0.745180406209397&y1=-0.09918019019976565&r=64&g=101&b=183&rd=1.155&gd=2.712&bd=2.736
+// http://gn00ltnd8rs6ph2/toys/mandel/mandel.html?i=2000&x0=-1.324537048165773&y0=-0.11084059575241267&x1=-1.3244185574612568&y1=-0.11077288677840344&h=0.408&hd=0.007
+// http://gn00ltnd8rs6ph2/toys/mandel/mandel.html?i=5000&x0=-1.3244585164555798&y0=-0.11083181825122136&x1=-1.3244584830263892&y1=-0.11083179914882679&r=99&g=50&b=58&rd=6.331&gd=4.037&bd=1.24
 
 $(function() {
    new MandelPage('#plane', '#plane-overlay');
@@ -27,6 +29,7 @@ class MandelPage {
    workerCount = 8;
    nextRow     = 0;
    zoomFactor  = 1.75;
+   colorMethod = 0;
 
    constructor(canvas, overlay) {
       this.InitAttributes(canvas, overlay);
@@ -72,12 +75,18 @@ class MandelPage {
    }
 
    InitColors() {
-      this.cdelta = 10; // (256 * 15)/(this.maxSteps - this.minSteps);
-   
-      this.colors = {
-         r: this.RandomI(255), rd: this.Random(this.cdelta),
-         g: this.RandomI(255), gd: this.Random(this.cdelta),
-         b: this.RandomI(255), bd: this.Random(this.cdelta)
+      if (this.colorMethod) {
+         this.colors = { // for hsl based coloring
+            h:  this.Random(1),
+            hd: this.Random(0.01) + 0.005,
+         }
+      } else {
+         let cdelta = 10; // (256 * 15)/(this.maxSteps - this.minSteps);
+         this.colors = { // for rgb based coloring
+            r: this.RandomI(255), rd: this.Random(cdelta),
+            g: this.RandomI(255), gd: this.Random(cdelta),
+            b: this.RandomI(255), bd: this.Random(cdelta),
+         }
       }
    }
 
@@ -112,6 +121,7 @@ class MandelPage {
       this.iter = {min: 99999, max: 0};
       this.NextInterlace(this.canvas.height);
       this.StartWorkers();
+      this.GenerateUrl();
    }
 
    SetupWorkers() {
@@ -144,7 +154,7 @@ class MandelPage {
          range   : this.range,
          row     : nextRow,
          maxIter : this.maxIter,
-         colors  : this.colors
+         colors  : this.colors,
       });
    }
    
@@ -174,9 +184,8 @@ class MandelPage {
    HandleKeyDown = (event) => {
       let e = event.originalEvent;
       switch(e.which){
-         case 13: return this.GenerateUrl();  // <enter> - generate a URL
-         case 32: return this.CenterAtPoint();// <space> - re color
-         case 67: return this.Recolor();      // c - re color
+         case 32: return this.CenterAtPoint();// <space> - center at cursor
+         case 67: return this.Recolor(e);     // c - re color
          case 68: return this.DebugInfo();    // d - debug info
          case 73: return this.ShowMaxIter();  // i - set max iteration
          case 75: return this.ColorPicker();  // k - color picker
@@ -311,7 +320,8 @@ class MandelPage {
       this.Draw();
    }
 
-   Recolor() {
+   Recolor(e) {
+      this.colorMethod = !!e.shiftKey;
       this.InitColors();
       this.Draw();
    }
@@ -334,10 +344,12 @@ class MandelPage {
          g: $('#color-table .gs').val(), gd: $('#color-table .gd').val(),
          b: $('#color-table .bs').val(), bd: $('#color-table .bd').val()
       }
+      this.colorMethod = 0;
       this.Draw();
    }
 
    Reset() {
+      history.replaceState(null, '', document.location.origin + document.location.pathname);
       this.range = {x0: -2.5, y0:-1, x1:1, y1: 1};
       this.HandleResize();
    }
@@ -431,7 +443,6 @@ class MandelPage {
       $('#from-y'     ).text(this.range.y0);
       $('#to-x'       ).text(this.range.x1);
       $('#to-y'       ).text(this.range.y1);
-      $('#cdelta'     ).text(this.cdelta);
       $('#workers'    ).text(this.workerCount);
       $('#render-time').text(this.renderTime);
       if (toggle) $('#debug-info' ).toggle();
@@ -444,9 +455,10 @@ class MandelPage {
       for (let p of ['x0','y0','x1','y1']) {
          this.range[p] = this.UrlParam(p, this.range[p])-0;
       }
-      for (let p of ['r','g','b','rd','gd','bd']) {
-         this.colors[p] = this.UrlParam(p, this.colors[p])-0;
+      for (let p of ['r','g','b','rd','gd','bd','h','hd']) {
+         this.colors[p] = this.UrlParam(p, this.colors[p] || 0)-0;
       }
+      this.colorMethod = !!this.colors.hd;
    }
 
    UrlParam(name, defaultVal) {
@@ -459,10 +471,14 @@ class MandelPage {
 
    GenerateUrl = () => {
       let s = self.state;
-      let url = new URL(document.location);
+      let url = new URL(document.location.pathname, document.location.origin);
+
       this.range.i = this.maxIter;
       ['i','x0','y0','x1','y1'].map(p => url.searchParams.set(p, this.range[p]));
-      ['r','g','b','rd','gd','bd'].map(p => url.searchParams.set(p, this.colors[p]));
+
+      let params = this.colorMethod ? ['h','hd'] : ['r','g','b','rd','gd','bd'];
+      params.map(p => url.searchParams.set(p, this.colors[p]));
+
       history.replaceState(null, '', url);
    }
 
